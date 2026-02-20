edd8bd24ed5bfc898d9947923fcfe08468e13866
_Avvake_ <denisergo18@gmail.com>
2026-02-20 12:24:33 +0200

    added config and docker-compose

---
+++ src/core/config.py
@@ -0,0 +1,36 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel, AmqpDsn
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+class DBConfig(BaseModel):
+
+    postgres_user: str
+    postgres_password: str
+    postgres_host: str
+    postgres_port: int
+    postgres_db: str
+    echo: bool = False
+
+    @property
+    def dsn(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@"
+            f"{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+        )
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+        env_prefix="APP_CONFIG__",
+        env_nested_delimiter="__",
+        case_sensitive=False,
+    )
+
+    db: DBConfig
+
+
+settings = Settings()
